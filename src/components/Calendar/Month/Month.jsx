cb5e582f3c1b69d9b7c70a39c8769318c9030dff
anton <antopavlu@gmail.com>
2021-10-29 14:02:14 +0300

    fix month start

--- src/components/Calendar/Month/Month.jsx
+++ src/components/Calendar/Month/Month.jsx
@@ -1,6 +1,5 @@
 import {
   addDays,
-  getMonth,
   getWeeksInMonth,
   startOfMonth,
   startOfWeek,
@@ -9,33 +8,13 @@ import React from "react";
 import Week from "../Week/Week";
 
 const Month = (props) => {
-  const firstDayCounter = (i) => {
-    if (i === 0) {
-      return 0;
-    }
-    else if (i === 1) {
-      return 7;
-    }
-    else if(i === 2) {
-      return 14;
-    }
-    else if (i === 3) {
-      return 21;
-    }
-    else if (i === 4) {
-      return 28;
-    }
-    else if (i === 5) {
-      return 35;
-    }
-  };
 
   const { currentDate } = props;
   const amountOfWeek = getWeeksInMonth(currentDate);
   const startingDay = startOfWeek(startOfMonth(currentDate));
 
   const weeks = new Array(amountOfWeek).fill(null).map((_, i) => {
-    return <Week key={i} firstDayOfWeek={addDays(startingDay, firstDayCounter(i))} />;
+    return <Week key={i} firstDayOfWeek={addDays(startingDay, i * 7)} />;
   }); 
 
   return <div>{weeks}</div>;
